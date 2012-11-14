1c9dcc18f324a123a9476e63ce25e2f52db8f347
HP <ranjeetbassi>
2012-11-14 13:28:30 -0500

    -committing the following 	-modified week 6 application of java ee application

---
+++ week6modified/src/sheridan/User.java
@@ -0,0 +1,54 @@
+package sheridan;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private long id;
+	private String username;
+	private String password;
+	private String email;
+	private Date birthDate;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+}
