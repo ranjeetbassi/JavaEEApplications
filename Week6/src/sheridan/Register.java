647b624ee9da3447a27a930a5d141534bc08684e
HP <ranjeetbassi>
2012-11-14 13:25:34 -0500

    -committing the following 	- week 6 of java ee application

---
+++ Week6/src/sheridan/Register.java
@@ -0,0 +1,32 @@
+package sheridan;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@ViewScoped
+public class Register implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private User user;
+
+	public Register() {
+		user = new User();
+
+	}
+
+	public void submit() {
+
+		FacesMessage message = new FacesMessage("Registration successful");
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+}
